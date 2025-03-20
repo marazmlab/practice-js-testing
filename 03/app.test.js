987d5c186101528a1ae21137f47f9db3e9f6ff85
Marek Bełz <belzmarek@gmail.com>
2025-03-20 16:21:52 +0000

    TASK03 test_03

--- 03/app.test.js
+++ 03/app.test.js
@@ -2,8 +2,12 @@ import randomNumber from './app';
 
 it('should return 1 when range is from 1 to 1', () => {
     expect(randomNumber(1, 1)).toBe(1);
-})
+});
 
 it('should throw error when arguments are not a numbers', () => {
     expect(() => randomNumber('arbuz', 1)).toThrow();
-})
+});
+
+it('should throw error when min is greater than max', () => {
+    expect(() => randomNumber(4, 3)).toThrow();
+});
