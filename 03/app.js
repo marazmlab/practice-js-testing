9db4583b4d0af1b173053caa6353af899bc7f345
Marek Bełz <belzmarek@gmail.com>
2025-03-20 16:14:24 +0000

    TASK03 implement_02

--- 03/app.js
+++ 03/app.js
@@ -1,4 +1,7 @@
 export default function randomNumber(min, max) {
+    if (typeof min !== 'number' || typeof max !== 'number') {
+        throw new Error('arguments are not a numbers')
+    }
     if (min === max) {
         return min;
     }
