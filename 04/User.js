01503fbf4e8dc7075d9369a06958ba8e857ea38a
Marek Bełz <belzmarek@gmail.com>
2025-03-20 17:04:12 +0000

    task 04 done

--- 04/User.js
+++ 04/User.js
@@ -1,3 +1,24 @@
 export default class User {
+    constructor({ email, password }) {
+        if (!email.includes('@')) {
+            throw new Error('Invalid email');
+        }
+        if (password.length < 6) {
+            throw new Error('Invalid password');
+        }
+        this.email = email;
+        this.password = password;
+    }
 
+    getEmail() {
+        return this.email;
+    }
+
+    getPassword() {
+        return this.password;
+    }
+
+    login() {
+        return this.email.includes('devmentor.pl');
+    }
 }
